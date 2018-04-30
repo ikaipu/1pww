b5e78b50a331aa4616c525050ae0e42077d7abde
ikaipu <kabikabitznz@gmail.com>
2018-04-30 16:09:13 +0800

    initial commit

---
+++ Assets/Module/ChatNode/Impl/ChatNodeModel.cs
@@ -0,0 +1,13 @@
+using MySystem.MVP.Interface;
+
+namespace MyModule.ChatNode.Impl
+{
+    public class ChatNodeModel : BaseModel<ChatNodeModel, ChatNodeView, ChatNodePresenter> 
+    {
+        public override void OnShow()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
---
+++ Assets/Module/ChatNode/Impl/ChatNodePresenter.cs
@@ -0,0 +1,8 @@
+using MySystem.MVP.Interface;
+namespace MyModule.ChatNode.Impl
+{
+    public class ChatNodePresenter : BasePresenter<ChatNodeModel, ChatNodeView, ChatNodePresenter> 
+    {
+
+    }
+}
