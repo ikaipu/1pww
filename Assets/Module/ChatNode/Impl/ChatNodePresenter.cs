119d7f76875a1c463266ca525dffb9e3fcda7452
ikaipu <kabikabitznz@gmail.com>
2018-05-08 22:03:12 +0800

    change name of game object

--- Assets/Module/ChatNode/Impl/ChatNodePresenter.cs
+++
@@ -1,8 +0,0 @@
-using MySystem.MVP.Interface;
-namespace MyModule.ChatNode.Impl
-{
-    public class ChatNodePresenter : BasePresenter<ChatNodeModel, ChatNodeView, ChatNodePresenter> 
-    {
-
-    }
-}
---
+++ Assets/Scripts/ListViewController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ListViewController : MonoBehaviour
+{
+
+	public RectTransform chatNodePrefab;
+
+	private void Start()
+	{
+		Instantiate(chatNodePrefab);
+		for (var i = 0; i < 15; i++)
+		{
+			var chatNodeInst = Instantiate(chatNodePrefab);
+			chatNodeInst.SetParent(transform, false);
+
+			var text = chatNodeInst.GetComponentInChildren<Text>();
+			text.text = "item:" + i;
+		}
+	}
+}
